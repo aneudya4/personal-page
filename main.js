c9f63fa4ed5310d667a8422a120e858c999f469f
Aneudy Adames <a4aneudy@gmail.com>
2020-12-09 00:05:05 -0500

    minified files

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-document.addEventListener('DOMContentLoaded', ()=>{const toggleSwitch=document.querySelector('nav input'); const sunIcon=document.querySelector('nav .icon-sun'); const moonIcon=document.querySelector('nav .icon-moon'); const switchThemes=(event)=>{const theme={}; if (toggleSwitch.checked){theme.dark=true; localStorage.setItem('theme', JSON.stringify(theme)); document.documentElement.setAttribute('data-theme', 'dark'); sunIcon.style.display='none'; moonIcon.style.display='inline';}else{theme.dark=false; localStorage.setItem('theme', JSON.stringify(theme)); document.documentElement.setAttribute('data-theme', 'light'); sunIcon.style.display='inline'; moonIcon.style.display='none';}}; toggleSwitch.addEventListener('change', switchThemes); const theme=JSON.parse(localStorage.getItem('theme')); if (theme){if (!theme.dark){toggleSwitch.checked=false; switchThemes();}}});
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleSwitch = document.querySelector('nav input');
+  const sunIcon = document.querySelector('nav .icon-sun');
+  const moonIcon = document.querySelector('nav .icon-moon');
+  const switchThemes = (event) => {
+    const theme = {};
+    if (toggleSwitch.checked) {
+      theme.dark = true;
+      localStorage.setItem('theme', JSON.stringify(theme));
+      document.documentElement.setAttribute('data-theme', 'dark');
+      sunIcon.style.display = 'none';
+      moonIcon.style.display = 'inline';
+    } else {
+      theme.dark = false;
+      localStorage.setItem('theme', JSON.stringify(theme));
+      document.documentElement.setAttribute('data-theme', 'light');
+      sunIcon.style.display = 'inline';
+      moonIcon.style.display = 'none';
+    }
+  };
+  toggleSwitch.addEventListener('change', switchThemes);
+  const theme = JSON.parse(localStorage.getItem('theme'));
+  if (theme) {
+    if (!theme.dark) {
+      toggleSwitch.checked = false;
+      switchThemes();
+    }
+  }
+});
