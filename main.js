ea7d5729a60e6e3261fbb27ff7eb727137641702
Aneudy Adames <a4aneudy@gmail.com>
2020-12-10 01:54:54 -0500

    testing new route for submitted forms

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-document.addEventListener('DOMContentLoaded', ()=>{const body=document.querySelector('body'); const toggleSwitch=document.querySelector('nav input'); const navLinks=document.querySelectorAll('.overlay-content a'); const hamburgerIcon=document.querySelector('.hamburger-menu'); const overlay=document.querySelector('.overlay'); const sunIcon=document.querySelector('nav .icon-sun'); const moonIcon=document.querySelector('nav .icon-moon'); const openMenu=()=>{body.classList.toggle('overflow-el'); hamburgerIcon.classList.toggle('open'); overlay.classList.toggle('open');}; const switchThemes=(event)=>{const theme={}; if (toggleSwitch.checked){theme.dark=true; localStorage.setItem('theme', JSON.stringify(theme)); document.documentElement.setAttribute('data-theme', 'dark'); sunIcon.style.display='none'; moonIcon.style.display='inline';}else{theme.dark=false; localStorage.setItem('theme', JSON.stringify(theme)); document.documentElement.setAttribute('data-theme', 'light'); sunIcon.style.display='inline'; moonIcon.style.display='none';}}; hamburgerIcon.addEventListener('click', openMenu); toggleSwitch.addEventListener('change', switchThemes); navLinks.forEach((link)=>{link.addEventListener('click', openMenu);}); const theme=JSON.parse(localStorage.getItem('theme')); if (theme){if (!theme.dark){toggleSwitch.checked=false; switchThemes();}}});
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const toggleSwitch = document.querySelector('nav input');
+  const navLinks = document.querySelectorAll('.overlay-content a');
+  const hamburgerIcon = document.querySelector('.hamburger-menu');
+  const overlay = document.querySelector('.overlay');
+  const sunIcon = document.querySelector('nav .icon-sun');
+  const moonIcon = document.querySelector('nav .icon-moon');
+  const openMenu = () => {
+    body.classList.toggle('overflow-el');
+    hamburgerIcon.classList.toggle('open');
+    overlay.classList.toggle('open');
+  };
+  const switchThemes = (event) => {
+    const theme = {};
+    if (toggleSwitch.checked) {
+      theme.dark = true;
+      localStorage.setItem('theme', JSON.stringify(theme));
+      document.documentElement.setAttribute('data-theme', 'dark');
+      sunIcon.style.display = 'none';
+      moonIcon.style.display = 'inline';
+    } else {
+      theme.dark = false;
+      localStorage.setItem('theme', JSON.stringify(theme));
+      document.documentElement.setAttribute('data-theme', 'light');
+      sunIcon.style.display = 'inline';
+      moonIcon.style.display = 'none';
+    }
+  };
+  hamburgerIcon.addEventListener('click', openMenu);
+  toggleSwitch.addEventListener('change', switchThemes);
+  navLinks.forEach((link) => {
+    link.addEventListener('click', openMenu);
+  });
+  const theme = JSON.parse(localStorage.getItem('theme'));
+  if (theme) {
+    if (!theme.dark) {
+      toggleSwitch.checked = false;
+      switchThemes();
+      return;
+    }
+  }
+});
