20f72b2b9f7c8f61a1c7f2c5ecb03ca7343c7ebd
Aneudy Adames <a4aneudy@gmail.com>
2021-07-13 21:55:09 -0400

    changed the layout for the banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import img from '../images/office-4.svg';
 export default function Banner() {
     return (
         <section className="banner">
             <div className="banner__left">
                 <div className="banner__info">
                     <span>Hello I'm</span>
-                    <h2>Aneudy Adames</h2>
+                    <h2>
+                        Aneudy <span>Adames</span>
+                    </h2>
                     <p>
                         a web developer based in NYC with a passion for building
                         high-quality, thoughtful, and intuitive web
@@ -16,7 +19,9 @@ export default function Banner() {
                 </div>
             </div>
             <div className="banner__right">
-                <div className="banner__img-container"></div>
+                <div className="banner__img-container">
+                    <img src={img} alt="react" />
+                </div>
             </div>
         </section>
     );
