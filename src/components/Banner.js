2f0ec7fbf3597a90dce61747d2eeae513c8b757d
Aneudy Adames <a4aneudy@gmail.com>
2021-07-15 12:01:56 -0400

    adjusted the banners position

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,29 +4,31 @@ import { StaticImage } from 'gatsby-plugin-image';
 export default function Banner() {
     return (
         <section className="banner">
-            <div className="banner__left">
-                <div className="banner__info">
-                    <span>Hello I'm</span>
-                    <h2>
-                        Aneudy <span>Adames</span>
-                    </h2>
-                    <p>Full-Stack Web Developer</p>
-                    <p>
-                        a web developer based in NYC with a passion for building
-                        high-quality, thoughtful, and intuitive web
-                        applications.
-                    </p>
+            <div className="banner__container">
+                <div className="banner__left">
+                    <div className="banner__info">
+                        <span>Hello I'm</span>
+                        <h2>
+                            Aneudy <span>Adames</span>
+                        </h2>
+                        <p>Full-Stack Web Developer</p>
+                        <p>
+                            a web developer based in NYC with a passion for
+                            building high-quality, thoughtful, and intuitive web
+                            applications.
+                        </p>
 
-                    <button className="btn banner__btn">Hire Me</button>
+                        <button className="btn banner__btn">Hire Me</button>
+                    </div>
                 </div>
-            </div>
-            <div className="banner__right">
-                <StaticImage
-                    src="../images/office-4.svg"
-                    placeholder="tracedSVG"
-                    className="banner__img-container"
-                    alt="developer"
-                />
+                <div className="banner__right">
+                    <StaticImage
+                        src="../images/office-4.svg"
+                        placeholder="tracedSVG"
+                        className="banner__img-container"
+                        alt="developer"
+                    />
+                </div>
             </div>
         </section>
     );
